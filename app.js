c1c901d43a63e9db1be76442b76574249c7730e2
Shivam Mishra <shivamalpha@localhost.localdomain>
2020-07-14 03:21:26 +0530

    v1

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector('div.banner button').addEventListener('click', () => {
+	document.querySelector('.banner').style.display='none';
+	document.querySelector('.slideshow-wrapper').style.cssText ='opacity: 1; visibility: visible';
+	document.querySelector('.slideshow').style.animation= 'slideshow 24s infinite';
+});
